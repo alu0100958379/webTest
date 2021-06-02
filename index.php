5509dd28676cb37f380e7140d22b1fa10fb0fc0d
alu0100958379 <alu0100958379@ull.edu.es>
2021-06-02 18:46:42 +0100

    cambios

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<html>
 <?php 
 include "functions.php"
 ?>
@@ -33,8 +34,6 @@ include "functions.php"
 		}
 	}
 ?>
-
-<html>
 	<head>
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 		<link rel="stylesheet" href="css/custom-style.css">
